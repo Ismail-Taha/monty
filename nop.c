635c1be5cfc25c9995c755760138da3de2365aac
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-11-15 12:10:44 -0800

    betty

--- nop.c
+++ nop.c
@@ -1,5 +1,12 @@
 #include "monty.h"
 
+/**
+ * nop - Does nothing. No operation.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number in the Monty bytecode file.
+ *
+ * Return: Void.
+ */
 void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
