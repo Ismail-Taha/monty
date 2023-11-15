8e034f1430bef19ae8fca1dd9f849595cb50cb86
Joe-0009 <youssefrachidi1994@gmail.com>
2023-11-15 15:15:14 -0800

    gg

--- swap.c
+++ swap.c
@@ -5,23 +5,18 @@
  * @line_number: no used
  * Return: no return
 */
+
 void swap(stack_t **stack, unsigned int line_number)
 {
-	stack_t *first = *stack;
-	stack_t *second = (*stack)->next;
+	int temp;
 
-	if (*stack == NULL)
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	first->next = second->next;
-	first->prev = second;
-	second->next = first;
-	second->prev = NULL;
-	if (first->next != NULL)
-		first->next->prev = first;
-
-	*stack = second;
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
 }
