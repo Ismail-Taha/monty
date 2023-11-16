9ee4093788d41c15a154a3db0a04826fd4dd8304
Joe-0009 <youssefrachidi1994@gmail.com>
2023-11-16 01:39:33 -0800

    sub

--- sub.c
+++ sub.c
@@ -7,7 +7,7 @@
  *
  * Return: Void.
  */
-void add(stack_t **stack, unsigned int line_number)
+void sub(stack_t **stack, unsigned int line_number)
 {
 
 	int sub = 0;
@@ -16,7 +16,7 @@ void add(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
